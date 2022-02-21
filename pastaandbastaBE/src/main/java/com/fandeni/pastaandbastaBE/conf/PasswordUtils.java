1694fc965c2cfed1c5dcaba3489e7143b413f4e9
TheBeck13 <alagia30@gmail.com>
2022-02-21 20:04:48 +0100

    Fixed password hashing using BCryptPasswordEncoder

--- pastaandbastaBE/src/main/java/com/fandeni/pastaandbastaBE/conf/PasswordUtils.java
+++ pastaandbastaBE/src/main/java/com/fandeni/pastaandbastaBE/conf/PasswordUtils.java
@@ -1,6 +1,5 @@
 package com.fandeni.pastaandbastaBE.conf;
 
-import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
--- pastaandbastaBE/src/main/java/com/fandeni/pastaandbastaBE/service/UtenteService.java
+++ pastaandbastaBE/src/main/java/com/fandeni/pastaandbastaBE/service/UtenteService.java
@@ -4,18 +4,25 @@ import com.fandeni.pastaandbastaBE.conf.PasswordUtils;
 import com.fandeni.pastaandbastaBE.customException.NoUserFoundExceptioin;
 import com.fandeni.pastaandbastaBE.model.Utente;
 import com.fandeni.pastaandbastaBE.repository.UtenteCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UtenteService {
+    //PasswordUtils.encrypt generates a different hash based on the time,
+    // but like this I cannot compare the two hash and see if password is correct, so I use BCryptPasswordEncoder
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+    @Autowired
     private UtenteCrudRepository utenteCrudRepository;
+
     public boolean checkLoginUsername(String username, String password) throws NoUserFoundExceptioin {
         Optional<Utente> optionalUtente = utenteCrudRepository.findByUsername(username);
         if(optionalUtente.isPresent()){
             Utente u = optionalUtente.get();
-            return PasswordUtils.encrypt(password).equals(u.getPassword());
+            return passwordEncoder.encode(password).equals(u.getPassword());
         }else{
             throw new NoUserFoundExceptioin("Wrong username!");
         }
